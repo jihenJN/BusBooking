8328812a31b3b0e7a77385769e0f9703ce794f9e
jihenJN <jihenjihen23@gmail.com>
2025-05-17 20:11:42 +0100

    feat : integrate fake API get

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { MasterService } from '../../services/master.service';
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './search.component.html',
-  styleUrl: './search.component.css'
+  styleUrl: './search.component.css',
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  locations: Observable<any[]> = new Observable<any[]>();
 
+  masterService = inject(MasterService);
+
+  ngOnInit(): void {
+    this.getAllLocations();
+  }
+
+  getAllLocations() {
+    this.locations = this.masterService.getLocations();
+  }
 }
