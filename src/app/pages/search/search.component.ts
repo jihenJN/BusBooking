86157536bb3dcc50522225ca78cf21bebf11f774
jihenJN <jihenjihen23@gmail.com>
2025-05-18 00:01:56 +0100

    feat: add search bus

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,17 +1,20 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MasterService } from '../../services/master.service';
 import { Observable } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, DatePipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [AsyncPipe],
+  imports: [AsyncPipe,FormsModule,DatePipe],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css',
 })
 export class SearchComponent implements OnInit {
   locations: Observable<any[]> = new Observable<any[]>();
+  searchParams :any ={fromLocation:'',toLocation:'',travelDate:''};
+  busesList:any[]=[];
 
   masterService = inject(MasterService);
 
@@ -21,5 +24,12 @@ export class SearchComponent implements OnInit {
 
   getAllLocations() {
     this.locations = this.masterService.getLocations();
+  }
+
+  onSearch(){
+    const  { fromLocation, toLocation, travelDate } = this.searchParams;
+    this.masterService.searchBuses(fromLocation, toLocation, travelDate ).subscribe((res:any)=>{
+      this.busesList=res;
+    })
   }
 }
--- src/app/services/master.service.ts
+++ src/app/services/master.service.ts
@@ -8,10 +8,14 @@ import { Observable } from 'rxjs';
 export class MasterService {
   apiUrl ='http://localhost:3000/'
   constructor(private http:HttpClient) {
-    
+
    }
 
    getLocations() : Observable<any[]>{
     return this.http.get<any[]>(this.apiUrl+'busLocations');
+   }
+
+   searchBuses(from:number,to:number,date:string) : Observable<any[]>{
+    return this.http.get<any[]>(`${this.apiUrl}buses?fromLocation=${from}&toLocation=${to}&travelDate=${date}`);
    }
 }
